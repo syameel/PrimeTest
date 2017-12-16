4dae826c4edf70efb190ee1bf3caec0072a8f07f
Ahmad Syameel <ahmadsyameel7@gmail.com>
2017-12-15 19:28:31 -0800

    prime.c: Add getPrime function
    Signed-off-by: Ahmad Syameel <ahmadsyameel7@gmail.com>

--- prime.c
+++ prime.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char*argv[]){
-printf("Hello there\n");
-return 0;
+
+
+int getPrime(int nthprime)
+{
+	int var1=100;
+
+	printf("Get Prime\n");
+
+	return var1;
+}
+
+
+int main(int argc, char*argv[])
+{
+
+	int result;
+	int nthprime=100;
+
+	printf("Hello there\n");
+
+	result=getPrime(nthprime);
+
+	printf("result=%d\n",result);
+
+	return 0;
 }
 
